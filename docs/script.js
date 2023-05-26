a651feef94e859a1534100e044b6ea7490521154
Lucas Kang <kangzundao@gmail.com>
2023-05-26 16:21:11 -0400

    first release

--- docs/script.js
+++ docs/script.js
@@ -5,3 +5,13 @@ function scrollFunc() {
     else
         document.getElementById('navbar').style.backgroundColor = "";
 }
+document.getElementById('menu').onclick = function() {
+    var navigation = document.getElementById('navigation');
+    if(navigation.style.display == "" || navigation.style.display == "none")
+        navigation.style.display = "block";
+    else
+        navigation.style.display = "none";
+};
+
+
+
